ff6326f6e8dc587f01b7133d49d4726223b1bad4
Adrian Varas <adrianvaras2001@gmail.com>
2023-03-27 12:53:28 +0200

    Agregar archivos de proyecto.

---
+++ Amiibopedia/Amiibopedia/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Amiibopedia.App;
+
+namespace Amiibopedia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+           
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+ 
+ 
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
